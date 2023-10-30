284881d484e8ff274931659e73c83738852179f3
sandesh kshirsagar <sandeshkshirsagar505@gmail.com>
2023-10-30 16:27:53 +0530

    Add project files.

---
+++ Serialization_Demo/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Serialization_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSalary = new System.Windows.Forms.Label();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.btnBinaryWrite = new System.Windows.Forms.Button();
+            this.btnSOAPWrite = new System.Windows.Forms.Button();
+            this.btnXMLWrite = new System.Windows.Forms.Button();
+            this.btnBinaryRead = new System.Windows.Forms.Button();
+            this.btnXMLRead = new System.Windows.Forms.Button();
+            this.btnSOAPRead = new System.Windows.Forms.Button();
+            this.btnJOSNWrite = new System.Windows.Forms.Button();
+            this.btnJOSNRead = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.BackColor = System.Drawing.Color.Transparent;
+            this.lblid.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblid.Location = new System.Drawing.Point(35, 81);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(19, 18);
+            this.lblid.TabIndex = 0;
+            this.lblid.Text = "id";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(35, 152);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(48, 18);
+            this.lblName.TabIndex = 1;
+            this.lblName.Text = "Name";
+            // 
+            // lblSalary
+            // 
+            this.lblSalary.AutoSize = true;
+            this.lblSalary.BackColor = System.Drawing.Color.Transparent;
+            this.lblSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalary.Location = new System.Drawing.Point(35, 227);
+            this.lblSalary.Name = "lblSalary";
+            this.lblSalary.Size = new System.Drawing.Size(49, 18);
+            this.lblSalary.TabIndex = 2;
+            this.lblSalary.Text = "Salary";
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(127, 77);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(128, 20);
+            this.txtid.TabIndex = 3;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(127, 148);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(128, 20);
+            this.txtName.TabIndex = 4;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Location = new System.Drawing.Point(127, 223);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(128, 20);
+            this.txtSalary.TabIndex = 5;
+            // 
+            // btnBinaryWrite
+            // 
+            this.btnBinaryWrite.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnBinaryWrite.Location = new System.Drawing.Point(340, 36);
+            this.btnBinaryWrite.Name = "btnBinaryWrite";
+            this.btnBinaryWrite.Size = new System.Drawing.Size(137, 35);
+            this.btnBinaryWrite.TabIndex = 6;
+            this.btnBinaryWrite.Text = "Binary Write";
+            this.btnBinaryWrite.UseVisualStyleBackColor = false;
+            this.btnBinaryWrite.Click += new System.EventHandler(this.btnBinaryWrite_Click);
+            // 
+            // btnSOAPWrite
+            // 
+            this.btnSOAPWrite.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnSOAPWrite.Location = new System.Drawing.Point(340, 165);
+            this.btnSOAPWrite.Name = "btnSOAPWrite";
+            this.btnSOAPWrite.Size = new System.Drawing.Size(137, 35);
+            this.btnSOAPWrite.TabIndex = 7;
+            this.btnSOAPWrite.Text = "SOAP Write";
+            this.btnSOAPWrite.UseVisualStyleBackColor = false;
+            this.btnSOAPWrite.Click += new System.EventHandler(this.btnSOAPWrite_Click);
+            // 
+            // btnXMLWrite
+            // 
+            this.btnXMLWrite.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnXMLWrite.Location = new System.Drawing.Point(340, 99);
+            this.btnXMLWrite.Name = "btnXMLWrite";
+            this.btnXMLWrite.Size = new System.Drawing.Size(137, 35);
+            this.btnXMLWrite.TabIndex = 8;
+            this.btnXMLWrite.Text = "XML Write";
+            this.btnXMLWrite.UseVisualStyleBackColor = false;
+            this.btnXMLWrite.Click += new System.EventHandler(this.btnXMLWrite_Click);
+            // 
+            // btnBinaryRead
+            // 
+            this.btnBinaryRead.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnBinaryRead.Location = new System.Drawing.Point(540, 36);
+            this.btnBinaryRead.Name = "btnBinaryRead";
+            this.btnBinaryRead.Size = new System.Drawing.Size(137, 35);
+            this.btnBinaryRead.TabIndex = 9;
+            this.btnBinaryRead.Text = "Birnary Read";
+            this.btnBinaryRead.UseVisualStyleBackColor = false;
+            this.btnBinaryRead.Click += new System.EventHandler(this.btnBinaryRead_Click);
+            // 
+            // btnXMLRead
+            // 
+            this.btnXMLRead.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnXMLRead.Location = new System.Drawing.Point(540, 99);
+            this.btnXMLRead.Name = "btnXMLRead";
+            this.btnXMLRead.Size = new System.Drawing.Size(137, 35);
+            this.btnXMLRead.TabIndex = 10;
+            this.btnXMLRead.Text = "XML Read";
+            this.btnXMLRead.UseVisualStyleBackColor = false;
+            this.btnXMLRead.Click += new System.EventHandler(this.btnXMLRead_Click);
+            // 
+            // btnSOAPRead
+            // 
+            this.btnSOAPRead.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnSOAPRead.Location = new System.Drawing.Point(540, 165);
+            this.btnSOAPRead.Name = "btnSOAPRead";
+            this.btnSOAPRead.Size = new System.Drawing.Size(137, 35);
+            this.btnSOAPRead.TabIndex = 11;
+            this.btnSOAPRead.Text = "SOAP Read";
+            this.btnSOAPRead.UseVisualStyleBackColor = false;
+            this.btnSOAPRead.Click += new System.EventHandler(this.btnSOAPRead_Click);
+            // 
+            // btnJOSNWrite
+            // 
+            this.btnJOSNWrite.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnJOSNWrite.Location = new System.Drawing.Point(340, 235);
+            this.btnJOSNWrite.Name = "btnJOSNWrite";
+            this.btnJOSNWrite.Size = new System.Drawing.Size(137, 35);
+            this.btnJOSNWrite.TabIndex = 12;
+            this.btnJOSNWrite.Text = "JOSN Write";
+            this.btnJOSNWrite.UseVisualStyleBackColor = false;
+            this.btnJOSNWrite.Click += new System.EventHandler(this.btnJOSNWrite_Click);
+            // 
+            // btnJOSNRead
+            // 
+            this.btnJOSNRead.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.btnJOSNRead.Location = new System.Drawing.Point(540, 235);
+            this.btnJOSNRead.Name = "btnJOSNRead";
+            this.btnJOSNRead.Size = new System.Drawing.Size(137, 35);
+            this.btnJOSNRead.TabIndex = 13;
+            this.btnJOSNRead.Text = "JOSN Read";
+            this.btnJOSNRead.UseVisualStyleBackColor = false;
+            this.btnJOSNRead.Click += new System.EventHandler(this.btnJOSNRead_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(721, 441);
+            this.Controls.Add(this.btnJOSNRead);
+            this.Controls.Add(this.btnJOSNWrite);
+            this.Controls.Add(this.btnSOAPRead);
+            this.Controls.Add(this.btnXMLRead);
+            this.Controls.Add(this.btnBinaryRead);
+            this.Controls.Add(this.btnXMLWrite);
+            this.Controls.Add(this.btnSOAPWrite);
+            this.Controls.Add(this.btnBinaryWrite);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.lblSalary);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblid);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblSalary;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Button btnBinaryWrite;
+        private System.Windows.Forms.Button btnSOAPWrite;
+        private System.Windows.Forms.Button btnXMLWrite;
+        private System.Windows.Forms.Button btnBinaryRead;
+        private System.Windows.Forms.Button btnXMLRead;
+        private System.Windows.Forms.Button btnSOAPRead;
+        private System.Windows.Forms.Button btnJOSNWrite;
+        private System.Windows.Forms.Button btnJOSNRead;
+    }
+}
+
---
+++ Serialization_Demo/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Windows.Forms;
+//Binary 
+using System.Runtime.Serialization.Formatters.Binary;
+//XML
+using System.Xml.Serialization;
+//SOAP
+using System.Runtime.Serialization.Formatters.Soap;
+//JSON
+using System.Text.Json;
+//File
+using System.IO;
+
+namespace Serialization_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBinaryWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //step 1-store data in the object
+                Employee employee = new Employee();
+                employee.Id = Convert.ToInt32(txtid.Text);
+                employee.Name = txtName.Text;
+                employee.Salary=Convert.ToDouble(txtSalary.Text);
+                //Step 2
+                FileStream fs=new FileStream(@"D:\emp.dat", FileMode.Create, FileAccess.Write);
+                //Step 3
+                BinaryFormatter bf = new BinaryFormatter();
+                bf.Serialize(fs, employee);
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch(Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnBinaryRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step 1
+                FileStream fs = new FileStream(@"D:\emp.dat", FileMode.Open, FileAccess.Read);
+                //Step 2
+                BinaryFormatter bf = new BinaryFormatter();
+                Employee employee = new Employee();
+                employee=(Employee)bf.Deserialize(fs);
+                //Step 3
+                txtid.Text=employee.Id.ToString();
+                txtName.Text = employee.Name; 
+                txtSalary.Text=employee.Salary.ToString();
+                fs.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXMLWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //step 1-store data in the object
+                Employee employee = new Employee();
+                employee.Id = Convert.ToInt32(txtid.Text);
+                employee.Name = txtName.Text;
+                employee.Salary = Convert.ToDouble(txtSalary.Text);
+                //Step 2
+                FileStream fs = new FileStream(@"D:\emp.xml", FileMode.Create, FileAccess.Write);
+                //Step 3
+                XmlSerializer xserializer = new XmlSerializer(typeof(Employee));
+                xserializer.Serialize(fs, employee);
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnXMLRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step 1
+                FileStream fs = new FileStream(@"D:\emp.xml", FileMode.Open, FileAccess.Read);
+                //Step 2
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(Employee));
+                Employee employee = new Employee();
+                employee = (Employee)xmlSerializer.Deserialize(fs);
+                //Step 3
+                txtid.Text = employee.Id.ToString();
+                txtName.Text = employee.Name;
+                txtSalary.Text = employee.Salary.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSOAPWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //step 1-store data in the object
+                Employee employee = new Employee();
+                employee.Id = Convert.ToInt32(txtid.Text);
+                employee.Name = txtName.Text;
+                employee.Salary = Convert.ToDouble(txtSalary.Text);
+                //Step 2
+                FileStream fs = new FileStream(@"D:\emp.soap", FileMode.Create, FileAccess.Write);
+                //Step 3
+                SoapFormatter formatter = new SoapFormatter();
+                formatter.Serialize(fs, employee);
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnSOAPRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step 1
+                FileStream fs = new FileStream(@"D:\emp.soap", FileMode.Open, FileAccess.Read);
+                //Step 2
+                SoapFormatter soapFormatter = new SoapFormatter();
+                Employee employee = new Employee();
+                employee = (Employee)soapFormatter.Deserialize(fs);
+                //Step 3
+                txtid.Text = employee.Id.ToString();
+                txtName.Text = employee.Name;
+                txtSalary.Text = employee.Salary.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnJOSNWrite_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //step 1-store data in the object
+                Employee employee = new Employee();
+                employee.Id = Convert.ToInt32(txtid.Text);
+                employee.Name = txtName.Text;
+                employee.Salary = Convert.ToDouble(txtSalary.Text);
+                //Step 2
+                FileStream fs = new FileStream(@"D:\emp.json", FileMode.Create, FileAccess.Write);
+                //Step 3
+                JsonSerializer.Serialize<Employee>(fs, employee);
+                fs.Close();
+                MessageBox.Show("Done");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        private void btnJOSNRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Step 1
+                FileStream fs = new FileStream(@"D:\emp.json", FileMode.Open, FileAccess.Read);
+                //Step 2
+                SoapFormatter soapFormatter = new SoapFormatter();
+                Employee employee = new Employee();
+                employee = JsonSerializer.Deserialize<Employee>(fs);
+                //Step 3
+                txtid.Text = employee.Id.ToString();
+                txtName.Text = employee.Name;      
+                txtSalary.Text = employee.Salary.ToString();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        } 
+
+       
+    }
+}
